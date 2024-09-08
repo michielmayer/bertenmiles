9c4aa764f9b915daff3d97f419785b47d70094ac
Michiel Maijer <michiel.mayer@kvk.nl>
2024-09-08 11:22:38 +0200

    bug fix

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3002;
 const server = require('http').createServer(app);
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
